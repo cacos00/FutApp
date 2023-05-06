4cd944b9dd6ef660fbf58f3a87a04f966670c4b0
Marcos <marcosfilho1095@gmail.com>
2023-05-06 03:25:55 -0300

    início projeto!

---
+++ src/main/java/com/project/FutApp/FutAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.FutApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/FutApp/FutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FutApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
